99addd151827a684491c383cff5492e24449fce3
Nelson Ameyo <nelson@lipasafe.com>
2020-09-16 15:13:47 +0300

    Adding videos

--- app/Models/Exhibitor.php
+++ app/Models/Exhibitor.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exhibitor extends Model
 {
-    //
+    function getLogoAttribute($val)
+    {
+        $val = str_replace('\\', '/', $val);
+        return asset('storage/' . $val);
+    }
+
+    function getBannerAttribute($val)
+    {
+        $val = str_replace('\\', '/', $val);
+        return asset('storage/' . $val);
+    }
 }
--- resources/views/exhibition/view.blade.php
+++ resources/views/exhibition/view.blade.php
@@ -1,33 +1,43 @@
 @extends('layout.frontend')
 
-<div class="container">
-    <div class="row">
+@section('content')
 
-        <div class="col-12 mb-5 mt-5">
-            <div class="row">
-                <div class="col-1 align-items-center pt-3">
-                    <img src="{{ $exhibitor->logo }}" class="img-fluid">
-                </div>
-                <div class="col-11">
-                    <h1 class="mt-1">{{ $exhibitor->business_name }}</h1>
-                    <a href="#">Download business profile</a>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12 mb-5 mt-5">
+                <div class="row">
+                    <div class="col-1 align-items-center pt-3">
+                        <img src="{{ $exhibitor->logo }}" class="img-fluid">
+                    </div>
+                    <div class="col-11">
+                        <h1 class="mt-1">{{ $exhibitor->business_name }}</h1>
+                        @php
+                            $val = json_decode($exhibitor->profile);
+                            $val = $val[0]->download_link;
+                            $val = str_replace('\\', '/', $val);
+                            $val = asset('storage/' . $val);
+                        @endphp
+                        <a target="_blank" href="{{ $val }}">Download
+                            business profile</a>
+                    </div>
                 </div>
             </div>
+
+            @if($exhibitor->banner)
+                <div class="col-12">
+                    <img src="{{ $exhibitor->banner }}" class="img-fluid">
+                </div>
+            @endif
+
+            <div class="col-md-12 mt-5">
+                <object data="{{ $val }}" type="application/pdf" height="800px"
+                        width="100%">
+                    <iframe src="{{ $val }}&embedded=true"></iframe>
+                </object>
+            </div>
+
+
         </div>
-
-
-        <div class="col-12">
-            <img src="{{ $exhibitor->banner }}" class="img-fluid">
-        </div>
-
-
-        <div class="col-md-12 mt-5">
-            <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" height="800px" width="100%">
-                <iframe src="http://africau.edu/images/default/sample.pdf&embedded=true"></iframe>
-            </object>
-        </div>
-
-
     </div>
-</div>
-
+@endsection
