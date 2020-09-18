0cc4939e0aa02d38f48c7410d9408eff1877848f
Nelson Ameyo <nelson@lipasafe.com>
2020-09-18 13:16:20 +0300

    hbfhgf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,21 +57,25 @@
         <div class="row">
             <div class="col-lg-6 mt-3">
                 <div class="ha-pic mt-5">
-                    <img src="{{ asset('/img/thanksgiving-3004005_1920.jpg') }}" alt="">
+                    <img src="{{ asset('/img/thanksgiving-3004005_1920.png') }}" alt="">
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="ha-text">
                     <h2>About Us</h2>
                     <p>
-                        The IREN Growthpad Virtual Food Festival 2020 aims to deliver a great African travel experience through promotion of indigenous foods and music. On September 24-25, 2020, the virtual food festival will feature Kamukunji panel sessions; Festival square for exhibitions, African Recipe Books, outcome of the Chakula Chetu Recipe Challenge 2020 and chat lounge.
+                        The IREN Growthpad Virtual Food Festival 2020 aims to deliver a great African travel experience
+                        through promotion of indigenous foods and music. On September 24-25, 2020, the virtual food
+                        festival will feature Kamukunji panel sessions; Festival square for exhibitions, African Recipe
+                        Books, outcome of the Chakula Chetu Recipe Challenge 2020 and chat lounge.
                     </p>
                     <p>
                         Join the adventure by clicking the register button! Register
                     </p>
 
                     <a href="/register" class="btn btn-success">Register Here!</a>
-                    <a target="_blank" href="/The IREN Growthpad Virtual Food Festival 2020 Program.pdf" class="btn btn-primary">Our Programme</a>
+                    <a target="_blank" href="/The IREN Growthpad Virtual Food Festival 2020 Program.pdf"
+                        class="btn btn-primary">Our Programme</a>
                 </div>
             </div>
         </div>
@@ -94,10 +98,10 @@
         </div>
     </div>
     @php
-        $speakers = \App\Models\Speaker::get();
+    $speakers = \App\Models\Speaker::get();
     @endphp
     @foreach ($speakers as $speaker)
-        @include('components.speaker')
+    @include('components.speaker')
     @endforeach
 
 </section>
