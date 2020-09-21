312ed3d7b8b91f12f8b7557efba667df059fd34e
Nelson Ameyo <nelson@lipasafe.com>
2020-09-21 21:44:34 +0300

    Adding videos

--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -3,8 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Manup Template">
-    <meta name="keywords" content="Manup, unica, creative, html">
+    <meta name="description" content="{{ config('app.name') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
