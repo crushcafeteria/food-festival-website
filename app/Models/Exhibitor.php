eb67b69c298709a6dfd521be05f60df0d13d9702
Nelson Ameyo <nelson@lipasafe.com>
2020-09-23 17:27:24 +0300

    Adding videos

--- app/Models/Exhibitor.php
+++ app/Models/Exhibitor.php
@@ -6,23 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exhibitor extends Model
 {
-    function getLogoAttribute($val)
-    {
-        if ($val) {
-            $val = str_replace('\\', '/', $val);
-            return asset('storage/' . $val);
-        }
-
-        return null;
-    }
-
-    function getBannerAttribute($val)
-    {
-        if ($val) {
-            $val = str_replace('\\', '/', $val);
-            return asset('storage/' . $val);
-        }
-
-        return null;
-    }
+//    function getLogoAttribute($val)
+//    {
+//        if ($val) {
+//            $val = str_replace('\\', '/', $val);
+//            return asset('storage/' . $val);
+//        }
+//
+//        return null;
+//    }
+//
+//    function getBannerAttribute($val)
+//    {
+//        if ($val) {
+//            $val = str_replace('\\', '/', $val);
+//            return asset('storage/' . $val);
+//        }
+//
+//        return null;
+//    }
 }
